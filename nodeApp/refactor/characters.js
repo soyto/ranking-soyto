92eefbec128f5f6d576fc50415e781c8110d84b2
soyto <alf.ixos@gmail.com>
2018-05-31 17:23:08 +0200

    ((DEV) refactor must look for all characters) (0.0.36)

--- nodeApp/refactor/characters.js
+++ nodeApp/refactor/characters.js
@@ -29,24 +29,44 @@ Character.prototype.update = function() {
 
       for(let _server of _servers.map(x => x.split('\.')[0])) {
         let _t = (new Date()).getTime();
-
         let _serverInfo = await $fsData.server.get(_date, _server);
 
         if(!_serverInfo || !_serverInfo.serverName) { continue; }
 
-        for(let $$characterEntry of _serverInfo.elyos.concat(_serverInfo.asmodians)) {
+        let _characterIDs = [];
+
+        try {
+          _characterIDs = (await $fs.readdir($path.join($config.folders.characters, _serverInfo.serverName))).map(x => parseInt(x.split('.')[0]));
+        } catch(err) {}
+
+        let _serverCharacters =  _serverInfo.elyos.concat(_serverInfo.asmodians);
+        let _serverCharacterIDs = _serverCharacters.map(x => x.characterID);
+        let _IDsNotStored = _characterIDs.filter(x => _serverCharacterIDs.indexOf(x) < 0);
+        let _num = 0;
+
+        //Process ranked characters
+        for(let $$characterEntry of _serverCharacters) {
           let _character = await $fsData.character.update(_serverInfo.date, _serverInfo.serverName, $$characterEntry.characterID, $$characterEntry);
           await $fsData.character.store(_serverInfo.serverName, _character.characterID, _character);
+          _num++;
+        }
+
+        //Now process unranked ones
+        for(let $$characterID of _IDsNotStored) {
+          let _character = await $fsData.character.update(_serverInfo.date, _serverInfo.serverName, $$characterID, null);
+          await $fsData.character.store(_serverInfo.serverName, _character.characterID, _character);
+          _num++;
         }
 
         let _dotsLength = 15 - _server.length;
 
-        $log.debug('Processed %s:%s %s => %s [%s]',
+        $log.debug('Processed %s:%s %s => %s [%s]{%s}',
           colors.cyan(_date),
           colors.green(_server),
           Array.apply(null, {'length': _dotsLength}).map(x => '.').join(''),
           colors.magenta( (new Date()).getTime() - _t + ' ms'),
-          colors.yellow((process.memoryUsage().rss / 1024 / 1024 ).toFixed(2) + ' MB')
+          colors.yellow((process.memoryUsage().rss / 1024 / 1024 ).toFixed(2) + ' MB'),
+          colors.grey(_num)
         );
       }
 
