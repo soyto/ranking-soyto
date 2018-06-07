85288e9a3515de8608bd8815e3bda30bb4125964
soyto <alf.ixos@gmail.com>
2018-06-07 22:35:27 +0200

    ((DEV) Facebook share must work now) (0.0.81)

--- nodeApp/express-routes/seo/index.js
+++ nodeApp/express-routes/seo/index.js
@@ -5,6 +5,7 @@ const $log = require('../../helpers').log;
 const $config = require('../../config');
 const express = require('express');
 const $path = require('path');
+const Handlebars = require('handlebars');
 
 let router = express.Router();
 
@@ -25,6 +26,19 @@ fs.readdirSync(__dirname).forEach(file => {
   });
 
   router.use('/' + _name, require($path.join(__dirname, file)));
+});
+
+router.get('/', async (req, res) =>  {
+  try {
+    let _template = Handlebars.compile(await $fs.read($path.join($config.folders.templates, 'seo', 'serverInfo_share.hbs')));
+
+    let _result = _template();
+
+    return res.send(_result);
+  } catch(error) {
+    $log.error('Error > %o', error);
+    return res.status(500);
+  }
 });
 
 
--- server.js
+++ server.js
@@ -3,12 +3,6 @@ const $config = require('./nodeApp/config');
 const express = require('express');
 const app = express();
 
-//Logger middleware
-app.use((req, res, next) => {
-  $log.debug('Received -> %s', req.url);
-  next();
-});
-
 app.use('/seo/', require('./nodeApp/express-routes/seo'));
 
 app.listen($config.server.port, () => {
