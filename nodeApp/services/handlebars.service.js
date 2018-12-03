2027dc3522600b6826f409ff569654fddf8a7398
soyto <alf.ixos@gmail.com>
2018-12-03 16:57:32 +0100

    (#3)(DEV) Basic admin index

--- nodeApp/services/handlebars.service.js
+++ nodeApp/services/handlebars.service.js
@@ -25,37 +25,20 @@
     }
 
     /**
-     * Renders Index
+     * Render all files
      * @return {Promise.<void>}
      */
-    async renderIndex() {
-      let _templateInputLocation = $path.join($config.folders.templates, 'www', 'index.hbs');
-      let _templateOutputLocation = $path.join($config.folders.www, 'index.html');
-
-      let _data = {
-        'app_scripts': null
-      };
-
-      if($config.server.debug_mode) {
-        _data.app_scripts = $config.files.app_files.map(_mapScript);
-      }
-      else {
-        _data.app_scripts = [$config.files.uglify_dest].map(_mapScript);
-      }
-
-      return await _render(_templateInputLocation, _templateOutputLocation, _data);
-    }
-
-
     async renderAll() {
 
+      //First of all, register handlebars partials
+      await _registerHandlebarsPartials();
 
       for(let file of $config.files.nodeApp_hbs_files) {
 
         //Main index file has some special needs
         if(file == $path.join($config.folders.templates, 'www', 'index.hbs')) {
           $log.debug('Rendering [%s]', $path.join($config.folders.www, 'index.html'));
-          await this.renderIndex();
+          await _renderIndex();
         }
         else {
 
@@ -65,6 +48,9 @@
 
           let _outputFilePath = _inputFilePath;
           let _outputFileName = _inputFileName;
+
+          //If file starts with underscore, is just a partial
+          if(_inputFileName.indexOf('_') === 0) { continue; }
 
           _outputFilePath = _outputFilePath.replace(
             $path.join($config.folders.templates, 'www', 'templates'),
@@ -96,6 +82,48 @@
    */
   function _mapScript(file) {
     return file.replace('www', '').replace(/\\/g, '/');
+  }
+
+  /**
+   * Renders index script
+   * @return {Promise.<void>}
+   * @private
+   */
+  async function _renderIndex() {
+    let _templateInputLocation = $path.join($config.folders.templates, 'www', 'index.hbs');
+    let _templateOutputLocation = $path.join($config.folders.www, 'index.html');
+
+    let _data = {
+      'app_scripts': null
+    };
+
+    if($config.server.debug_mode) {
+      _data.app_scripts = $config.files.app_files.map(_mapScript);
+    }
+    else {
+      _data.app_scripts = [$config.files.uglify_dest].map(_mapScript);
+    }
+
+    return await _render(_templateInputLocation, _templateOutputLocation, _data);
+  }
+
+  /**
+   * Register handlebars partials
+   * @return {Promise.<void>}
+   * @private
+   */
+  async function _registerHandlebarsPartials() {
+    for(let file of $config.files.nodeApp_hbs_files) {
+
+      let _fileName = file.split('/').splice(file.split('/').length - 1)[0];
+
+      if(_fileName.indexOf('_') === 0) {
+        let _content = await $fs.read(file);
+        let _filePath = file.replace($path.join($config.folders.templates, 'www', 'templates'), '');
+
+        Handlebars.registerPartial(_filePath, _content);
+      }
+    }
   }
 
 
