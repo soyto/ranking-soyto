b54ef637ea7cf6e0083832aa051b2a64f946dcbc
soyto <alf.ixos@gmail.com>
2018-12-04 16:23:38 +0100

    (#3)(DEV) character scrapper changed to use database

--- nodeApp/express-routes/scrapper/character.js
+++ nodeApp/express-routes/scrapper/character.js
@@ -1,56 +1,62 @@
-const $fs = require('../../helpers').fs;
-const $path = require('path');
-const $config = require('../../../config');
-const $fsData = require('../../fsData');
-const $log = require('../../helpers').log;
-const $seo = require('../../helpers').seo;
-const express = require('express');
-const Handlebars = require('handlebars');
+(() => {
+  const $fs = require('../../helpers').fs;
+  const $path = require('path');
+  const $config = require('../../../config');
+  const $fsData = require('../../fsData');
+  const $log = require('../../helpers').log;
+  const $seo = require('../../helpers').seo;
+  const express = require('express');
+  const Handlebars = require('handlebars');
+  const characterService = require('../../services').character;
 
-let router = express.Router();
+  let router = express.Router();
 
 
-/**
- * Print handlebars template
- */
-router.get('/:serverName/:characterID', async (req, res) => {
-  try {
-    let _characterData = await $fsData.character.get(req.params.serverName, req.params.characterID);
+  /**
+   * Print handlebars template
+   */
+  router.get('/:serverName/:characterID', async (req, res) => {
 
-    if(!_characterData) {
-      return res.status(404).end();
+    try {
+      let _characterData = await $fsData.character.get(req.params.serverName, req.params.characterID);
+
+      if (!_characterData) {
+        return res.status(404).end();
+      }
+
+      let _dbCharacterData = await characterService.get(req.params.serverName, req.params.characterID);
+
+      let _template = Handlebars.compile(await $fs.read($path.join($config.folders.templates, 'scrapper', 'characterInfo.hbs')));
+
+      let _result = _template({
+        'title': $seo.character.title(_characterData),
+        'description': $seo.character.description(_characterData),
+        'keywords': $seo.character.keywords(_characterData),
+        'character': _characterData,
+        'picture': _getPicture(_dbCharacterData.profile_pic_url, _characterData),
+      });
+
+      res.send(_result);
+
+    } catch (error) {
+      $log.error('Error > %o', error);
+      return res.status(500).end();
     }
+  });
 
-    let _characterPic = (await $fs.readJSON($config.files.characterPics)).filter(x => x.characterID == req.params.characterID && x.serverName == req.params.serverName).shift();
-    let _template = Handlebars.compile(await $fs.read($path.join($config.folders.templates, 'scrapper', 'characterInfo.hbs')));
-
-    let _result = _template({
-      'title': $seo.character.title(_characterData),
-      'description': $seo.character.description(_characterData),
-      'keywords': $seo.character.keywords(_characterData),
-      'character': _characterData,
-      'picture': _getPicture(_characterPic, _characterData),
-    });
-
-    res.send(_result);
-
-  } catch(error) {
-    $log.error('Error > %o', error);
-    return res.status(500).end();
+  function _getPicture(characterPic, characterData) {
+    if (characterPic) {
+      return characterPic;
+    }
+    else if (characterData) {
+      return '//placehold.it/450X300/DD66DD/EE77EE/?text=' + encodeURI(characterData.characterName);
+    }
+    else {
+      return '//placehold.it/450X300/DD66DD/EE77EE/?text=NotFound';
+    }
   }
-});
-
-function _getPicture(characterPic, characterData) {
-  if(characterPic) {
-    return characterPic.pic;
-  }
-  else if(characterData) {
-    return '//placehold.it/450X300/DD66DD/EE77EE/?text=' + encodeURI(characterData.characterName);
-  }
-  else {
-    return '//placehold.it/450X300/DD66DD/EE77EE/?text=NotFound';
-  }
-}
 
 
-module.exports = router;
+  module.exports = router;
+
+})();
--- nodeApp/express-routes/scrapper/index.js
+++ nodeApp/express-routes/scrapper/index.js
@@ -1,47 +1,40 @@
-/* global require, module, __dirname */
-const fs = require('fs');
-const $fs = require('../../helpers').fs;
-const $log = require('../../helpers').log;
-const $seo = require('../../helpers').seo;
-const $config = require('../../../config');
-const express = require('express');
-const $path = require('path');
-const Handlebars = require('handlebars');
+(() => {
 
-let router = express.Router();
+  const fs = require('fs');
+  const $fs = require('../../helpers').fs;
+  const $log = require('../../helpers').log;
+  const $seo = require('../../helpers').seo;
+  const $config = require('../../../config');
+  const express = require('express');
+  const $path = require('path');
+  const Handlebars = require('handlebars');
 
-fs.readdirSync(__dirname).forEach(file => {
+  let router = express.Router();
 
-  //We only want .js files
-  if(!file.endsWith('.js')) { return; }
+  router.use('/character', require('./character'));
+  router.use('/ranking', require('./ranking'));
 
-  let _name = file.substr(0, file.length - 3);
+  router.get('/', async (req, res) =>  {
+    try {
+      let _template = Handlebars.compile(await $fs.read($path.join($config.folders.templates, 'scrapper', 'index.hbs')));
 
-  //If name is index, avoid
-  if(_name == 'index') { return; }
+      let _result = _template({
+        'title': $seo.index.title(),
+        'description': $seo.index.description(),
+        'keywords': $seo.keywords(),
+      });
 
-  router.use('/' + _name, require($path.join(__dirname, file)));
-});
-
-router.get('/', async (req, res) =>  {
-  try {
-    let _template = Handlebars.compile(await $fs.read($path.join($config.folders.templates, 'scrapper', 'index.hbs')));
-
-    let _result = _template({
-      'title': $seo.index.title(),
-      'description': $seo.index.description(),
-      'keywords': $seo.keywords(),
-    });
-
-    return res.send(_result);
-  } catch(error) {
-    $log.error('Error > %o', error);
-    return res.status(500);
-  }
-});
+      return res.send(_result);
+    } catch(error) {
+      $log.error('Error > %o', error);
+      return res.status(500);
+    }
+  });
 
 
-router.use('/', express.static('www'));
+  router.use('/', express.static('www'));
 
-module.exports = router;
+  module.exports = router;
+
+})();
 
